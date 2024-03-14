3c94fc6dc995d4e23ef677028793076c64079e00
showgo-koinuma <van71413989i@gmail.com>
2024-03-14 17:52:59 +0900

    [add] mainシーン設定

--- Assets/MyGame/Scripts/GameStateSystem/GameState/ResultState.cs
+++ Assets/MyGame/Scripts/GameStateSystem/GameState/ResultState.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// リザルト
@@ -9,17 +10,23 @@ public class ResultState : GameStateBase
     [SerializeField] private GameObject _resultPanel;
     [SerializeField] private GameObject _cakeLayoutGroup;
     [SerializeField] private ResultCakeContller _resultCakePrefab;
+    [SerializeField] private Text _totalPriceText;
     
     private CancellationToken _ct;
     
     public override void OnEnter(CancellationToken ct)
     {
+        Debug.Log("result enter");
         _resultPanel.SetActive(true);
+        int totalPrice = 0;
 
         foreach (var cakeData in InGameManager.Instance.FinishedCakeDatas)
         {
             Instantiate(_resultCakePrefab, _cakeLayoutGroup.transform).InitializeCake(cakeData.Color, cakeData.Price);
+            totalPrice += cakeData.Price;
         }
+
+        _totalPriceText.text = totalPrice.ToString();
     }
 
     public override void OnUpdate(float deltaTime)
