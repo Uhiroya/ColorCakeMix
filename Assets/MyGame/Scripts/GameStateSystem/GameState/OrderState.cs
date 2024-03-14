c876268d813bac6a379e5a27e9ee8598d35103d9
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2024-03-14 17:21:42 +0900

    客スライディング

--- Assets/MyGame/Scripts/GameStateSystem/GameState/OrderState.cs
+++ Assets/MyGame/Scripts/GameStateSystem/GameState/OrderState.cs
@@ -1,14 +1,26 @@
 using System.Threading;
+using DG.Tweening;
+using UnityEngine;
 
 /// <summary>
 /// 注文フェーズ
 /// </summary>
 public class OrderState : GameStateBase
 {
+    [SerializeField, Tooltip("客の画像")] private RectTransform _customer;
+    [SerializeField, Tooltip("客の開始地点")] private float _startXPosition;
+    [SerializeField, Tooltip("客がスライドし終わるまでの時間")] private float _duration;
     private CancellationToken _ct;
     public override void OnEnter(CancellationToken ct)
     {
-        
+        var pos = _customer.anchoredPosition;
+        pos.x = _startXPosition;
+        _customer.anchoredPosition = pos;
+        _customer.DOAnchorPosX(0, _duration)
+            .OnComplete(() =>
+            {
+                GameStateMachine.Instance.ChangeNextState(GamePhase.SelectMaterial);
+            }).SetLink(gameObject);
     }
 
     public override void OnUpdate(float deltaTime)
