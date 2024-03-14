39d4f41da34581943a17b504c44600e4a2351770
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-03-14 16:13:59 +0900

    [update] SelectStateの処理追加

--- Assets/MyGame/Scripts/GameStateSystem/GameState/SelectMaterialState.cs
+++ Assets/MyGame/Scripts/GameStateSystem/GameState/SelectMaterialState.cs
@@ -1,22 +1,55 @@
 using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Takechi;
+using ColorUtility = Takechi.ColorUtility;
 
 /// <summary>
 /// 色選択フェーズ
 /// </summary>
 public class SelectMaterialState : GameStateBase
 {
+    [SerializeField] private HotCakeView _cakeView;
     private CancellationToken _ct;
+
     public override void OnEnter(CancellationToken ct)
     {
+        _ct = ct;
+        SelectColorAsync();
+        JudgeMaterial();
+        GameStateMachine.Instance.ChangeNextState(GamePhase.Cook);
     }
 
     public override void OnUpdate(float deltaTime)
     {
-
     }
 
     public override void OnExit()
     {
+        _cakeView.Color1 = ColorManager.Instance.SelectMaterials[0];
+        _cakeView.Color2 = ColorManager.Instance.SelectMaterials[1];
+        _cakeView.Color3 = ColorManager.Instance.SelectMaterials[2];
+        ColorManager.Instance.SelectMaterials.Clear();
+        // TODO オーダーUIを非アクティブ化
+    }
 
+    private async void SelectColorAsync()
+    {
+        const int selectColorLimit = 3;
+        await UniTask.WaitUntil(() => selectColorLimit <= ColorManager.Instance.SelectMaterials.Count,
+            cancellationToken: _ct);
+    }
+
+    private void JudgeMaterial()
+    {
+        var colorManager = ColorManager.Instance;
+        Color colorSum = default;
+        foreach (var color in colorManager.SelectMaterials)
+        {
+            colorSum += color;
+        }
+
+        var colorScore = ColorUtility.ColorDistance(colorManager.OrderColor, colorSum / 3);
+        Debug.Log($"ColorScore {colorScore}");
     }
 }
